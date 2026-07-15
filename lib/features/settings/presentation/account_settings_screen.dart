c14e4b72fe47d20d65a9b5e5c5c9ad7689bde28f
Loan Rohou <124188367+lrohou@users.noreply.github.com>
2026-07-15 14:55:10 +0200

    Update account_settings_screen.dart

--- lib/features/settings/presentation/account_settings_screen.dart
+++ lib/features/settings/presentation/account_settings_screen.dart
@@ -63,7 +63,7 @@ class AccountSettingsScreen extends ConsumerWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => DabRadioScreen(),
+                  builder: (context) => const DabRadioScreen(),
                 ),
               );
             },
